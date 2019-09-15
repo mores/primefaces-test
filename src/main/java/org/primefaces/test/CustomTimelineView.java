d3bf29299979a30b6428689396969291c1ff7b5c
Stephen More <stephen.more@gmail.com>
2019-09-15 07:37:01 -0400

    Use case why a legend is needed.

--- src/main/java/org/primefaces/test/CustomTimelineView.java
+++ src/main/java/org/primefaces/test/CustomTimelineView.java
@@ -1,8 +1,11 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -19,6 +22,7 @@ public class CustomTimelineView implements Serializable {
     private TimelineModel model;
     private Date start;
     private Date end;
+    private Map<String,String> colors;
 
     @PostConstruct
     public void init() {
@@ -38,22 +42,48 @@ public class CustomTimelineView implements Serializable {
         // create timeline model
         model = new TimelineModel();
 
+	colors = new HashMap<>();
+	colors.put("unavailable", "#F03030");
+	colors.put("available", "#1AA11A" );
+	colors.put("maybe", "#FFA500" );
+	colors.put("ready", "#150578" );
+	colors.put("call", "#78C0E0" );
+	colors.put("order", "#AAA95A");
+
+	int job = 1; 
         for (String name : NAMES) {
             now = new Date();
             Date end = new Date(now.getTime() - 12 * 60 * 60 * 1000);
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 15; i++) {
                 Date start = new Date(end.getTime() + Math.round(Math.random() * 5) * 60 * 60 * 1000);
                 end = new Date(start.getTime() + Math.round(4 + Math.random() * 5) * 60 * 60 * 1000);
 
-                long r = Math.round(Math.random() * 2);
-                String availability = (r == 0 ? "Unavailable" : (r == 1 ? "Available" : "Maybe"));
+                int r = (int)Math.round(Math.random() * 5);
+		ArrayList<String> keyList = new ArrayList<String>(colors.keySet());
 
                 // create an event with content, start / end dates, editable flag, group name and custom style class
-                TimelineEvent event = new TimelineEvent(availability, start, end, true, name, availability.toLowerCase());
+                TimelineEvent event = new TimelineEvent("Job: " + job, start, end, true, name, keyList.get(r));
                 model.add(event);
+
+		job++;
             }
         }
+    }
+
+    public String getCss() {
+
+	    String css = "";
+	    for (Map.Entry<String, String> entry : colors.entrySet()) {
+		    String key = entry.getKey();
+		    String value = entry.getValue();
+
+		    css = css + "div." + key + " {\n";
+		    css = css + "background: " + value + " none !important;\n";
+		    css = css + "}\n";
+	    }
+
+	    return css;
     }
 
     public TimelineModel getModel() {
